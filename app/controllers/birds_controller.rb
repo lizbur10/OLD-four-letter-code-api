cb14844b9983827f44c3b7a6e2d93d25bcda61b1
lizbur10 <liz.burton147@gmail.com>
2020-04-11 08:16:14 -0400

    added endpoints for code quiz app

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,8 +5,28 @@ class BirdsController < ApplicationController
         render json: birds
     end
 
+    def random
+        rando = Bird.all.sample
+        render json: rando
+    end
+
+    def appledore
+        appledore_birds = get_appledore_birds
+        render json: appledore_birds
+    end
+
+    def appledore_random
+        appledore_rando = get_appledore_birds.sample
+        render json: appledore_rando
+    end
+
     def show
         bird = Bird.find(params[:id])
         render json: bird
     end
+
+    def get_appledore_birds
+        Bird.all.select { | bird | bird.appledore }
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/birds/random', to: 'birds#random'
+  get '/birds/appledore', to: 'birds#appledore'
+  get '/birds/appledore/random', to: 'birds#appledore_random'
   resources :birds
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
